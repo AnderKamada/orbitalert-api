08ba4037491255f4ca9725750cc355a3218f1c99
Ander Kamada <anderkamada3@gmail.com>
2026-05-26 22:27:31 -0300

    Adicionando projeto base, inicialmente com as dependencias do Spring Boot e a conexao com o PostgreSQL.

---
+++ src/test/java/com/orbitalert/api/OrbitalertApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orbitalert.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbitalertApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
