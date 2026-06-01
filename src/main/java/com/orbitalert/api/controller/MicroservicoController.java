36058c017f8ab05214057de83623b2fefaf56ad7
Ander Kamada <anderkamada3@gmail.com>
2026-06-01 18:05:23 -0300

    feat: implementa comunicacao entre microsservicos

---
+++ src/main/java/com/orbitalert/api/controller/MicroservicoController.java
@@ -0,0 +1,40 @@
+package com.orbitalert.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@RestController
+public class MicroservicoController {
+
+    @GetMapping("/microservico/status")
+    public String status()
+            throws IOException, InterruptedException {
+
+        HttpClient client =
+                HttpClient.newHttpClient();
+
+        HttpRequest request =
+                HttpRequest.newBuilder()
+                        .uri(
+                                URI.create(
+                                        "http://localhost:8081/status"
+                                )
+                        )
+                        .GET()
+                        .build();
+
+        HttpResponse<String> response =
+                client.send(
+                        request,
+                        HttpResponse.BodyHandlers.ofString()
+                );
+
+        return response.body();
+    }
+}
